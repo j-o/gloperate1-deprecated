9bc60fbbc7898465d581e0bf3d3a70319583ac1a
Jan Ole Vollmer <jan.vollmer@student.hpi.uni-potsdam.de>
2015-06-18 17:38:31 +0200

    Bugfixes for VirtualTimeControlWidget

--- source/gloperate-qtwidgets/include/gloperate-qtwidgets/VirtualTimeControlWidget.h
+++ source/gloperate-qtwidgets/include/gloperate-qtwidgets/VirtualTimeControlWidget.h
@@ -19,12 +19,14 @@ namespace gloperate_qtwidgets
 
 class TimePlayerWidget;
 
-class VirtualTimeControlWidget : public widgetzeug::DockableScrollAreaWidget
+class GLOPERATE_QTWIDGETS_API VirtualTimeControlWidget : public widgetzeug::DockableScrollAreaWidget
 {
     Q_OBJECT
 
 public:
-    VirtualTimeControlWidget(gloperate::VirtualTimeController * controller, QWidget * parent = nullptr);
+    VirtualTimeControlWidget(gloperate::VirtualTimeController * controller, const QString & title, QWidget * parent = nullptr, Qt::WindowFlags flags = 0);
+    VirtualTimeControlWidget(gloperate::VirtualTimeController * controller, QWidget * parent = nullptr, Qt::WindowFlags flags = 0);
+    virtual ~VirtualTimeControlWidget();
 
 private:
     std::unique_ptr<TimePlayerWidget> m_timePlayerWidget;
--- source/gloperate-qtwidgets/source/VirtualTimeControlWidget.cpp
+++ source/gloperate-qtwidgets/source/VirtualTimeControlWidget.cpp
@@ -9,11 +9,21 @@ using namespace widgetzeug;
 namespace gloperate_qtwidgets
 {
 
-VirtualTimeControlWidget::VirtualTimeControlWidget(gloperate::VirtualTimeController * controller, QWidget * parent)
-:   DockableScrollAreaWidget(parent)
+VirtualTimeControlWidget::VirtualTimeControlWidget(gloperate::VirtualTimeController * controller, const QString & title, QWidget * parent, Qt::WindowFlags flags)
+:   DockableScrollAreaWidget(title, parent, flags)
 ,   m_timePlayerWidget(new TimePlayerWidget(controller, this))
 {
+    m_timePlayerWidget->setWindowTitle("");
     addWidget(m_timePlayerWidget.get());
+}
+
+VirtualTimeControlWidget::VirtualTimeControlWidget(gloperate::VirtualTimeController * controller, QWidget * parent, Qt::WindowFlags flags)
+:   VirtualTimeControlWidget(controller, "", parent, flags)
+{
+}
+
+VirtualTimeControlWidget::~VirtualTimeControlWidget()
+{
 }
 
 } /* namespace gloperate_qtwidgets */
